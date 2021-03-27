876ebb9ac4cb5307055043d791df246a039b6bb2
ZyuC <m9t1k3zczc@gmail.com>
2021-03-28 00:54:25 +0900

    カーネルを読み込む部分を記述

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -6,6 +6,7 @@
 #include <Protocol/SimpleFileSystem.h>
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
+#include <Guid/FileInfo.h>
 
 
 struct MemoryMap{
@@ -131,6 +132,23 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
 
 	SaveMemoryMap(&memmap, memmap_file);
 	memmap_file->Close(memmap_file);
+
+
+	EFI_FILE_PROTOCOL* kernel_file;
+	root_dir->Open(root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
+
+	UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
+	UINT8 file_info_buffer[file_info_size];
+	kernel_file->GetInfo(kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer);
+
+	EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+	UINTN kernel_file_size = file_info->FileSize;
+
+	EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
+	gBS->AllocatePages(AllocateAddress, EfiLoaderData, (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+	kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+	Print(L"kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+
 
 	Print(L"All done.\n");
 
